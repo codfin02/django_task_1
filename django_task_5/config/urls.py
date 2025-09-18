e26e011274a93b00d4b823f6f145b170ff5730fd
codfin02 <codfin02@gmail.com>
2025-09-18 16:02:46 +0900

    django_task_add

---
+++ django_task_5/config/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import include, path
+from users import views as user_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # CBV Todo endpoints under /cbv/
+    path('cbv/', include('todo.urls')),
+
+    # Auth
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', user_views.login_view, name='login'),
+    path('accounts/signup/', user_views.sign_up, name='signup'),
+]
+
